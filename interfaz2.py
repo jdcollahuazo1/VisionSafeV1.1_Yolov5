661ef8bb629554a5039c749deb9b231774c4c45b
Losif <56968086+jdcollahuazo1@users.noreply.github.com>
2024-02-09 03:41:04 -0500

    Integracion Second Camera , Alert  & Sound

--- interfaz2.py
+++ interfaz2.py
@@ -1,10 +1,10 @@
 import tkinter as tk
-from tkinter import ttk
 import threading
 import torch
 import cv2
 import numpy as np
 from PIL import Image, ImageTk
+import chime
 
 import pathlib
 
@@ -15,78 +15,90 @@ pathlib.PosixPath = pathlib.WindowsPath
 class ObjectDetectorApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Sistema de Monitoreo VisioSafe v1.0")
+        self.root.title("Sistema de Monitoreo VisioSafe v1.1")
 
         # Establecer el tamaño de la ventana
         self.root.geometry("1280x720")
 
         # Agregar un fondo a la ventana
-        background_image = Image.open("img/disCam.png")  # Reemplaza con la ruta de tu imagen de fondo
+        background_image = Image.open("img/disCam3.png")  # Reemplaza con la ruta de tu imagen de fondo
         background_photo = ImageTk.PhotoImage(background_image)
 
         background_label = tk.Label(root, image=background_photo)
         background_label.image = background_photo
         background_label.place(relwidth=1, relheight=1)
 
-        # Label para mostrar el frame de la cámara USB
-        self.video_label_usb = tk.Label(root)
-        self.video_label_usb.place(x=13, y=29)  # Ajusta las coordenadas según tus necesidades
+        # Label para mostrar el frame de la CAM1
+        self.cam1_label = tk.Label(root)
+        self.cam1_label.place(x=13, y=29)  # Ajusta las coordenadas según tus necesidades
 
-        # Label para mostrar el frame de la cámara IP
-        self.video_label_ip = tk.Label(root)
-        self.video_label_ip.place(x=508, y=29)  # Ajusta las coordenadas según tus necesidades
+        # Label para mostrar el frame de la CAM 2
+        self.cam2_label = tk.Label(root)
+        self.cam2_label.place(x=508, y=29)  # Ajusta las coordenadas según tus necesidades
 
-        # Botón para iniciar la cámara USB
-        fondo_button = "#6AA84F"
-        self.start_usb_button = ttk.Button(root, text="Activar CAM1", command=self.start_usb_video)
-        self.start_usb_button.place(x=1077, y=135)
+        # Botón para iniciar la cámara CAM 1
+        self.active_cam1_button = tk.Button(root, text="Activar CAM1", command=self.active_cam1)
+        self.active_cam1_button.place(x=1080, y=175)
 
-        # Botón para iniciar la cámara IP
-        self.start_ip_button = ttk.Button(root, text="Activar CAM2", command=self.start_ip_video)
-        self.start_ip_button.place(x=1077, y=220)
+        # Botón para iniciar la cámara CAM2
+        self.active_cam2_button = tk.Button(root, text="Activar CAM2", command=self.active_cam2)
+        self.active_cam2_button.place(x=1080, y=210)
 
         self.model = torch.hub.load('ultralytics/yolov5', 'custom',
-                                    path='C:/Users/ASUS VIVOBOOK/PycharmProjects/VisionSafev5/model/best.pt')
+                                    path='model/best.pt')
 
-        self.cap_usb = None
-        self.cap_ip = None
-        self.thread_usb = None
-        self.thread_ip = None
-        self.is_running_usb = False
-        self.is_running_ip = False
+        # Label para mostrar la imagen de alerta
+        self.alert_label = tk.Label(root)
+        self.alert_label.place(x=1023, y=390)  # Posición de la alerta
 
-    def start_usb_video(self):
-        self.cap_usb = cv2.VideoCapture(1)
-        if not self.cap_usb.isOpened():
+        self.cap_cam1 = None
+        self.cap_cam2 = None
+        self.thread_cam1 = None
+        self.thread_cam2 = None
+        self.is_running_cam1 = False
+        self.is_running_cam2 = False
+
+    def active_cam1(self):
+        self.cap_cam1 = cv2.VideoCapture(1)
+        if not self.cap_cam1.isOpened():
             print("No se puede abrir la cámara USB")
             return
 
-        self.is_running_usb = True
-        self.thread_usb = threading.Thread(target=self.detect_objects_usb)
-        self.thread_usb.start()
+        self.is_running_cam1 = True
+        self.thread_cam1 = threading.Thread(target=self.detect_objects_cam1)
+        self.thread_cam1.start()
 
-    def start_ip_video(self):
-        #url_rtsp = 'http://192.168.100.57:4747/video'  # Reemplaza con la URL correcta de tu cámara IP
-        self.cap_ip = cv2.VideoCapture(2)
-        if not self.cap_ip.isOpened():
+    def active_cam2(self):
+        self.cap_cam2 = cv2.VideoCapture(2)
+        if not self.cap_cam2.isOpened():
             print("No se puede abrir la cámara RTSP")
             return
 
-        self.is_running_ip = True
-        self.thread_ip = threading.Thread(target=self.detect_objects_ip)
-        self.thread_ip.start()
+        self.is_running_cam2 = True
+        self.thread_cam2 = threading.Thread(target=self.detect_objects_cam2)
+        self.thread_cam2.start()
 
-    def detect_objects_usb(self):
-        while self.is_running_usb:
-            ret, frame = self.cap_usb.read()
+    def detect_objects_cam1(self):
+        while self.is_running_cam1:
+            ret, frame = self.cap_cam1.read()
 
             # Cambia el tamaño a 200x200
             resized_frame = cv2.resize(frame, (485, 331))
             # Realizamos la detección
             detect = self.model(resized_frame)
 
-            info = detect.pandas().xyxy[0]  # Obtenemos la información de la detección
-            print(info)  # Mostramos la información
+            info_cam1 = detect.pandas().xyxy[0]  # Obtenemos la información de la detección
+            print(info_cam1)  # Mostramos la información
+
+            if info_cam1['confidence'].max() >= 0.92:
+                chime.theme('pokemon')
+                chime.error(sync=True)
+
+                # Muestra la imagen de alerta
+                alert_image_cam1 = Image.open("img/alerCam1.png")  # Reemplaza con la ruta de tu imagen de alerta
+                alert_photo = ImageTk.PhotoImage(alert_image_cam1)
+                self.alert_label.config(image=alert_photo)
+                self.alert_label.image = alert_photo
 
             # Mostramos FPS
             rendered_frame = np.squeeze(detect.render())
@@ -94,19 +106,29 @@ class ObjectDetectorApp:
             img = Image.fromarray(rendered_frame)
             img = ImageTk.PhotoImage(img)
 
-            self.video_label_usb.img = img
-            self.video_label_usb.configure(image=img)
-            self.video_label_usb.update_idletasks()
+            self.cam1_label.img = img
+            self.cam1_label.configure(image=img)
+            self.cam1_label.update_idletasks()
 
-    def detect_objects_ip(self):
-        while self.is_running_ip:
-            ret, frame = self.cap_ip.read()
+    def detect_objects_cam2(self):
+        while self.is_running_cam2:
+            ret, frame = self.cap_cam2.read()
             resized_frame = cv2.resize(frame, (485, 331))
             # Realizamos la detección
             detect = self.model(resized_frame)
 
-            info = detect.pandas().xyxy[0]  # Obtenemos la información de la detección
-            print(info)  # Mostramos la información
+            info_cam2 = detect.pandas().xyxy[0]  # Obtenemos la información de la detección
+            print(info_cam2)  # Mostramos la información
+
+            if info_cam2['confidence'].max() >= 0.92:
+                chime.theme('pokemon')
+                chime.error(sync=True)
+
+                # Muestra la imagen de alerta
+                alert_image_cam2 = Image.open("img/alerCam2.png")  # Reemplaza con la ruta de tu imagen de alerta
+                alert_photo = ImageTk.PhotoImage(alert_image_cam2)
+                self.alert_label.config(image=alert_photo)
+                self.alert_label.image = alert_photo
 
             # Mostramos FPS
             rendered_frame = np.squeeze(detect.render())
@@ -114,9 +136,10 @@ class ObjectDetectorApp:
             img = Image.fromarray(rendered_frame)
             img = ImageTk.PhotoImage(img)
 
-            self.video_label_ip.img = img
-            self.video_label_ip.configure(image=img)
-            self.video_label_ip.update_idletasks()
+            self.cam2_label.img = img
+            self.cam2_label.configure(image=img)
+            self.cam2_label.update_idletasks()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
